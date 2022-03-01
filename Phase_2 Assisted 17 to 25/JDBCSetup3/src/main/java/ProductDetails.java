2ea2e42524056e5c66da7d27563e7cc7de84640c
vijay nayak Banavath <97591138+vijaynayakgithub@users.noreply.github.com>
2022-03-01 19:35:12 +0530

    Add files via upload

---
+++ Phase_2 Assisted 17 to 25/JDBCSetup3/src/main/java/ProductDetails.java
@@ -0,0 +1,76 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import com.ecommerce.DBConnection;
+import com.mysql.cj.jdbc.CallableStatement;
+
+/**
+ * Servlet implementation class ProductDetails
+ */
+public class ProductDetails extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ProductDetails() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+        
+        try {
+                 PrintWriter out = response.getWriter();
+                 out.println("<html><body>");
+                 
+                InputStream in = getServletContext().getResourceAsStream("/WEB-INF/config.properties");
+                Properties props = new Properties();
+                props.load(in);
+                
+                
+                DBConnection conn = new DBConnection(props.getProperty("url"), props.getProperty("userid"), props.getProperty("password"));
+                CallableStatement stmt = (CallableStatement) conn.getConnection().prepareCall("{call add_product(?, ?)}");
+                stmt.setString(1, "new product");
+                stmt.setBigDecimal(2, new BigDecimal(1900.50));
+                stmt.executeUpdate();
+                
+                out.println("Stored procedure has been executed.<Br>");
+                stmt.close();
+                
+                
+                out.println("</body></html>");
+                conn.closeConnection();
+                
+        } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+        } catch (SQLException e) {
+                e.printStackTrace();
+        }
+}
+
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
